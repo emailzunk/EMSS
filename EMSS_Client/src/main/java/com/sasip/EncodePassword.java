6423a9b0e21d3a3971f40866d3a9f41319cce654
emailzunk <emailzunk@gmail.com>
2019-12-24 13:34:07 +0000

    Initial load

---
+++ EMSS_Client/src/main/java/com/sasip/EncodePassword.java
@@ -0,0 +1,21 @@
+package com.sasip;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class EncodePassword
+{
+    public static void main(String arga[])
+    {
+        PasswordEncoder pe = new BCryptPasswordEncoder();
+
+        System.out.println(pe.encode("admin"));
+        System.out.println(pe.encode("peri"));
+        System.out.println(pe.encode("Ram"));
+        System.out.println(pe.encode("sasi"));
+
+
+        System.out.println(Integer.toBinaryString(1 + 1));
+
+    }
+}
---
+++ EMSS_Server/src/main/java/com/emss/EmssServerApplication.java
@@ -0,0 +1,13 @@
+package com.emss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmssServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(EmssServerApplication.class, args);
+    }
+}
+
